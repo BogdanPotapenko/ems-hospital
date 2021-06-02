8d236eaa0e5eb49ed7afd56cd4f32c455c2f0332
Bogdan Potapenko <21bogdan09@gmail.com>
2021-06-02 23:33:14 +0300

    added mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,14 @@ import $ from 'jquery';
 
 import MicroModal from 'micromodal';
 import mobileMenu from './modules/mobileMenu';
+import navbarScroll from './modules/navbarScroll';
 
 $(() => {
-  MicroModal.init();
+  MicroModal.init({
+    openTrigger: 'data-custom-open',
+    disableScroll: true,
+  });
+
   mobileMenu.init();
+  navbarScroll.init();
 });
--- src/js/modules/mobileMenu.js
+++ src/js/modules/mobileMenu.js
@@ -1,8 +1,21 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+const mobileMenu = (function x() {
+  const nav = document.querySelector('#body');
+  const menu = document.querySelector('#menu');
+  const menuToggle = document.querySelector('.nav-toggle');
+  let isMenuOpen = false;
+
+  function init() {
+    menuToggle.addEventListener('click', (e) => {
+      e.preventDefault();
+      isMenuOpen = !isMenuOpen;
+
+      menuToggle.setAttribute('aria-expanded', String(isMenuOpen));
+      menu.hidden = !isMenuOpen;
+      nav.classList.toggle('nav--open');
+    });
   }
+  return {
+    init,
+  };
+}());
+export default mobileMenu;
